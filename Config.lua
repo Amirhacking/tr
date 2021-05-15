aa32c5cb073839a1399f23be839a13c848802d2e
Amirhacking <41117749+Amirhacking@users.noreply.github.com>
2021-05-16 01:10:05 +0200

    Add files via upload

---
+++ Config.lua
@@ -0,0 +1,31 @@
+--[[ کانال سورس خونه ! پر از سورس هاي ربات هاي تلگرامي !
+لطفا در کانال ما عضو شويد 
+@source_home
+https://t.me/source_home ]]
+SUDO_ID = {358165791,1338641524,660335795} --آیدی سودو های ربات (ربات cli , api  قرار بدید)
+Full_Sudo = {358165791} -- آیدی سودو اصلی
+TD_ID = 1846248575 -- آیدی ربات
+Sudoid = 358165791 -- آیدی سودو اصلی
+BotHelper = 1827354436 -- آیدی هلپر
+UserBotHelper = '@CLEANERTESLABOT' -- یوزرنیم هلپر
+LinkSuppoRt = 'https://t.me/joinchat/9ndTVV-1PoNiMWRk' -- لینک گروه ساپورت
+UserSudo = '@amir_dev_lua' -- یوزرنیم سازنده
+PvUserSudo = '@amir_dev_lua' -- پیام رسان سازنده
+Channel = '@iranize_link' -- یوزرنیم کانال 
+Channell = '@ICEPHP'
+Channelll = '@ICEROBOTS'
+ChannelInline = 'iranize_link' --یوزنیم کانال بدون @
+RedisIndex = 4
+token = '1846248575:AAESnYnWZC2cnhMGL9ammmUqL5BoeLoXTm4' -- توکن ربات api
+--[[ کانال سورس خونه ! پر از سورس هاي ربات هاي تلگرامي !
+لطفا در کانال ما عضو شويد 
+@source_home
+https://t.me/source_home ]]
+--[[  آموزش نصب : 
+unzip MeGa+
+chmod +x MeGa+
+./MeGa+ install
+./MeGa+ config
+./MeGa+ login
+chmod +x Autolaunch
+screen ./Autolaunch ]]
